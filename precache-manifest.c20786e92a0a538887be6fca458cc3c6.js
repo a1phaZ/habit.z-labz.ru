48a8b23d318958b1533a0aede7472e61d6314e7d
Artemiy Zebzeev <artemiy.zebzeev@gmail.com>
2020-08-12 14:52:55 +0500

    Updates

---
+++ precache-manifest.c20786e92a0a538887be6fca458cc3c6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c776c397b97c90a2bbbcfcdde6e997f5",
+    "url": "/habit.z-labz.ru/index.html"
+  },
+  {
+    "revision": "9290ed5d307c6c255050",
+    "url": "/habit.z-labz.ru/static/css/2.1e562f28.chunk.css"
+  },
+  {
+    "revision": "9290ed5d307c6c255050",
+    "url": "/habit.z-labz.ru/static/js/2.1c67c626.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/habit.z-labz.ru/static/js/2.1c67c626.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "615536303c197501ecca",
+    "url": "/habit.z-labz.ru/static/js/main.a9ed6486.chunk.js"
+  },
+  {
+    "revision": "5731f99fbec715f635a5",
+    "url": "/habit.z-labz.ru/static/js/runtime-main.4636e08c.js"
+  }
+]);
