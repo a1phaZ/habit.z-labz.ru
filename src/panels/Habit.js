f7b6822c65f9fcf1b38010e5734dc398828d76c1
Artemiy Zebzeev <artemiy.zebzeev@gmail.com>
2020-08-21 12:04:26 +0500

    delete habit logic

--- src/panels/Habit.js
+++ src/panels/Habit.js
@@ -1,8 +1,8 @@
 import React, {useContext, useEffect, useState} from 'react';
-import {CellButton, List, PanelHeader, PanelHeaderBack} from "@vkontakte/vkui";
+import {Alert, CellButton, List, PanelHeader, PanelHeaderBack} from "@vkontakte/vkui";
 import {State} from "../state";
 import useApi from "../hooks/useApi";
-import {SET_CHANGE_HABIT, SET_HABIT_ID, SET_HISTORY_BACK} from "../state/actions";
+import {SET_CHANGE_HABIT, SET_HABIT_ID, SET_HABITS, SET_HISTORY_BACK, SET_POPOUT} from "../state/actions";
 import HabitBanner from "../components/HabitBanner";
 
 import Icon28ShareOutline from '@vkontakte/icons/dist/28/share_outline';
@@ -10,22 +10,24 @@ import Icon28CancelCircleOutline from '@vkontakte/icons/dist/28/cancel_circle_ou
 
 const HabitPage = ({habit, setHabit}) => {
 	const [, dispatch] = useContext(State);
-	const [{response}, doApiFetch] = useApi(`/habit/${habit._id}`);
+	const [{response}, doApiFetch] = useApi(`/habit/${habit?._id}`);
 
 	const [needFetch, setNeedFetch] = useState(false);
 
 	useEffect(() => {
 		if (!needFetch) return;
 		doApiFetch({
-			method: 'PUT'
+			method: 'DELETE'
 		});
 		setNeedFetch(false);
 	}, [needFetch, doApiFetch]);
 
 	useEffect(() => {
 		if (!response) return;
-		setHabit(response);
-	}, [response, setHabit]);
+		dispatch({type: SET_HABITS, payload: {habits: response}});
+		dispatch({type: SET_HISTORY_BACK});
+		// setHabit(response);
+	}, [response, dispatch]);
 
 	return (
 		<>
@@ -39,18 +41,44 @@ const HabitPage = ({habit, setHabit}) => {
 				/>
 			}
 			>
-				{habit.title}
+				{habit?.title}
 			</PanelHeader>
-			<HabitBanner habit={habit} dispatch={dispatch} isSingle={true} setNeedFetch={setNeedFetch}/>
+			<HabitBanner habit={habit} dispatch={dispatch} isSingle={true} setHabit={setHabit}/>
 			<List>
 				<CellButton
 					before={<Icon28ShareOutline/>}
 				>
-					{habit.days !== habit.daysComplete ? 'Поделиться прогрессом' : 'Поделиться успехом'}
+					{habit?.days !== habit?.daysComplete ? 'Поделиться прогрессом' : 'Поделиться успехом'}
 				</CellButton>
 				<CellButton
 					before={<Icon28CancelCircleOutline/>}
 					mode={'danger'}
+					onClick={() => {
+						dispatch({
+							type: SET_POPOUT, payload: {
+								popout: (
+									<Alert
+										actionsLayout="vertical"
+										actions={[{
+											title: 'Удалить',
+											autoclose: true,
+											mode: 'destructive',
+											action: () => setNeedFetch(true),
+										}, {
+											title: 'Отмена',
+											autoclose: true,
+											mode: 'cancel'
+										}]}
+										onClose={() => {dispatch({type: SET_POPOUT, payload: {popout: null}})}}
+									>
+										<h2>Подтвердите действие</h2>
+										<p>Вы уверены, что хотите удалить {habit?.title}?</p>
+										<p><i>Отменить удаление будет невозможно!</i></p>
+									</Alert>
+								)
+							}
+						});
+					}}
 				>
 					Удалить цель
 				</CellButton>
