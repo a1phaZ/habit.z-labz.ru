fd3adfb712acb89d9b1c9fc04a1dce07740d0631
Artemiy Zebzeev <artemiy.zebzeev@gmail.com>
2020-09-04 14:08:56 +0500

    add edit habit button

--- src/panels/Habit.js
+++ src/panels/Habit.js
@@ -3,11 +3,12 @@ import bridge from '@vkontakte/vk-bridge';
 import {Alert, CellButton, List, PanelHeader, PanelHeaderBack} from "@vkontakte/vkui";
 import {State} from "../state";
 import useApi from "../hooks/useApi";
-import {SET_CHANGE_HABIT, SET_HABIT_ID, SET_HABITS, SET_HISTORY_BACK, SET_POPOUT} from "../state/actions";
+import {SET_CHANGE_HABIT, SET_HABIT_ID, SET_HABITS, SET_HISTORY_BACK, SET_MODAL, SET_POPOUT} from "../state/actions";
 import HabitBanner from "../components/HabitBanner";
 
 import Icon28ShareOutline from '@vkontakte/icons/dist/28/share_outline';
 import Icon28CancelCircleOutline from '@vkontakte/icons/dist/28/cancel_circle_outline';
+import Icon28EditOutline from '@vkontakte/icons/dist/28/edit_outline';
 
 const HabitPage = ({habit, setHabit}) => {
 	const [, dispatch] = useContext(State);
@@ -43,7 +44,8 @@ const HabitPage = ({habit, setHabit}) => {
 					onClick={() => {
 						dispatch({type: SET_HISTORY_BACK});
 						dispatch({type: SET_HABIT_ID, payload: {habitId: null}});
-						dispatch({type: SET_CHANGE_HABIT, payload: {habit: habit}});
+						dispatch({type: SET_CHANGE_HABIT, payload: {habit: null}});
+						setHabit(null);
 					}}
 				/>
 			}
@@ -52,6 +54,12 @@ const HabitPage = ({habit, setHabit}) => {
 			</PanelHeader>
 			<HabitBanner habit={habit} dispatch={dispatch} isSingle={true} setHabit={setHabit}/>
 			<List>
+				<CellButton
+					before={<Icon28EditOutline/>}
+					onClick={() => {
+						dispatch({type: SET_MODAL, payload: {modal: 'add-habit'}});
+					}}
+				>Редактировать</CellButton>
 				<CellButton
 					before={<Icon28ShareOutline/>}
 					onClick={() => {
